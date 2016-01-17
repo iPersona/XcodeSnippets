626f31a0747f5f2a7a8d7d6366ad442d2c25926e
iPersona <freedom2028@gmail.com>
2016-01-17 22:56:17 +0800

    Add 'LoggerClass'

---
+++ LoggerClass.m
@@ -0,0 +1,33 @@
+---
+title: "Logger manage class based on 'CocoaLumberjack'."
+summary: "Create a SWIFT logger management class based on 'CocoaLumberjack'. Do remember to add '-DDEBUG' and '-DRELEASE' into the 'Project(NOT target)' -> 'Build Settings' -> 'Swift Compiler - Custom Flags'."
+completion-scope: Class Implementation
+---
+
+import UIKit
+import CocoaLumberjack
+
+class Logger: NSObject {
+    static let sharedInstance = Logger()
+    
+    private override init() {
+#if DEBUG
+        DDLog.setLevel(.Verbose, forClass: nil)
+#else
+        DDLog.setLevel(.Warning, forClass: nil)
+#endif
+    }
+    
+    func setup() {
+        // TTY
+        DDLog.addLogger(DDTTYLogger.sharedInstance())   // TTY = Xcode console
+        DDTTYLogger.sharedInstance().colorsEnabled = true   // Enable colorful console ('XcodeColors' plugin is required).
+        
+        // ASL
+        DDLog.addLogger(DDASLLogger.sharedInstance())   // ASL = Apple System Logs
+        
+        let fileLogger: DDFileLogger = DDFileLogger()   // File Logger
+        fileLogger.rollingFrequency = 60*60*24  // 24 hours
+        DDLog.addLogger(fileLogger)
+    }
+}
